0e9c1dd86b7f7803fdc8253688228e1661ba96d7
namlehong <namlehong@gmail.com>
2014-08-17 00:19:00 +0700

    debug ls

--- authserver/src/main/java/l2s/authserver/network/l2/c2s/RequestServerLogin.java
+++ authserver/src/main/java/l2s/authserver/network/l2/c2s/RequestServerLogin.java
@@ -31,9 +31,11 @@ public class RequestServerLogin extends L2LoginClientPacket
 	@Override
 	protected void runImpl()
 	{
+		System.out.println("RequestServerLogin");
 		L2LoginClient client = getClient();
 		if(!client.isPasswordCorrect())
 		{
+			System.out.println("RequestServerLogin 1");
 			client.close(LoginFailReason.REASON_USER_OR_PASS_WRONG);
 			return;
 		}
@@ -41,6 +43,7 @@ public class RequestServerLogin extends L2LoginClientPacket
 		SessionKey skey = client.getSessionKey();
 		if(skey == null || !skey.checkLoginPair(_loginOkID1, _loginOkID2))
 		{
+			System.out.println("RequestServerLogin 2");
 			client.close(LoginFailReason.REASON_ACCESS_FAILED);
 			return;
 		}
@@ -49,13 +52,17 @@ public class RequestServerLogin extends L2LoginClientPacket
 		GameServer gs = GameServerManager.getInstance().getGameServerById(_serverId);
 		if(gs == null || !gs.isAuthed() || gs.isGmOnly() && account.getAccessLevel() < 100 || gs.getOnline() >= gs.getMaxPlayers() && account.getAccessLevel() < 50)
 		{
+			System.out.println("RequestServerLogin 3");
 			client.close(LoginFailReason.REASON_ACCESS_FAILED);
 			return;
 		}
+		
+		System.out.println("RequestServerLogin 4");
 
 		account.setLastServer(_serverId);
 		account.update();
 
+		System.out.println("RequestServerLogin 5");
 		client.close(new PlayOk(skey));
 	}
 }
