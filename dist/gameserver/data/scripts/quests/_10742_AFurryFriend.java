b04621ffcfde1f518aee80f794c6b3b1b7817bd2
sontronghien <Hien Son>
2014-08-22 19:53:08 +0700

    Add script for quest 10742

--- dist/gameserver/data/scripts/quests/_10742_AFurryFriend.java
+++ dist/gameserver/data/scripts/quests/_10742_AFurryFriend.java
@@ -25,19 +25,15 @@ import l2s.gameserver.utils.ReflectionUtils;
 public class _10741_ADraughtForTheCold extends Quest implements ScriptFile
 {
 
-	private static final int SIVANTHE = 33951;
 	private static final int LEIRA = 33952;
+	private final static int KIKU_CAVE = 33995;
 	
-	private final static int HONEYBEE = 23452;
-	private final static int ROBUST_HONEYBEE = 23484;
-	private final static int KIKU = 23453;
-	
-	private final static int EMPTY_JAR  = 39527;
-	private final static int SWEET_HONEY  = 39528;
-	private final static int NUTRITIOUS_MEAT  = 39529;
+	private final static int RICKY = 19552;
 	
 	private static final int minLevel = 10;
 	private static final int maxLevel = 20;
+	
+	public static NpcInstance foxInstance = null;
 	
 	@Override
 	public void onLoad()
@@ -54,11 +50,9 @@ public class _10741_ADraughtForTheCold extends Quest implements ScriptFile
 	public _10741_ADraughtForTheCold()
 	{
 		super(false);
-		addStartNpc(SIVANTHE);
-		addTalkId(LEIRA);
+		addStartNpc(LEIRA);
+		addTalkId(KIKU_CAVE);
 		
-		addKillId(HONEYBEE, ROBUST_HONEYBEE, KIKU);
-
 		addLevelCheck(minLevel, maxLevel);
 		addRaceCheck(false, false, false, false, false, false, true);
 	}
@@ -69,27 +63,41 @@ public class _10741_ADraughtForTheCold extends Quest implements ScriptFile
 		String htmltext = event;
 		Player player = st.getPlayer();
 
-		if(event.equalsIgnoreCase("33951-2.htm"))
+		if(event.equalsIgnoreCase("33952-4.htm"))
 		{
 			st.setState(STARTED);
 			st.setCond(1);
 			st.playSound(SOUND_ACCEPT);
 		}
 		
-		if(event.equalsIgnoreCase("33952-2.htm"))
+		if(event.equalsIgnoreCase("check_fox"))
 		{
-			st.takeAllItems(SWEET_HONEY);
-			st.takeAllItems(NUTRITIOUS_MEAT);
-			
-			st.giveItems(ADENA_ID, 2000);
-			st.addExpAndSp(22973, 2);
-			
-			st.setState(COMPLETED);
-			st.exitCurrentQuest(false);
-			st.playSound(SOUND_FINISH);
-			
+			if((Math.random() < 0.3) && st.getInt("fox_spawn") != 1)
+			{
+				st.set("fox_spawn", 1);
+				
+				foxInstance = st.addSpawn(RICKY, 2*60000); //despawn after 2 mins
+				
+				st.startQuestTimer("fox_move", 2000);
+			}
 		}
 		
+		if(event.equalsIgnoreCase("fox_move"))
+		{
+			if(foxInstance == null) return;
+			
+			foxInstance.moveToLocation(player.getLoc(), 20, true);
+			
+			if(foxInstance.getLoc().distance(new Location(-78080, 237343, -3536)) > 100)
+			{
+				st.startQuestTimer("fox_move", 2000);
+			}
+			else
+			{
+				st.setCond(2);
+			}
+			
+		}
 		
 		return htmltext;
 	}
@@ -101,25 +109,36 @@ public class _10741_ADraughtForTheCold extends Quest implements ScriptFile
 		int cond = st.getCond();
 		int npcId = npc.getNpcId();
 		
-		if(npcId == SIVANTHE)
+		if(npcId == LEIRA)
 		{
 			if(checkStartCondition(st.getPlayer()))
 			{
-				htmltext = "33951-1.htm";
+				htmltext = "33952-1.htm";
 			}
 			else
 				htmltext = "noquest";
 			if(cond == 1)
 			{
-				htmltext = "33951-2.htm";
+				htmltext = "33952-5.htm";
+			}
+			else if(cond == 2)
+			{
+				st.giveItems(ADENA_ID, 2000);
+				st.addExpAndSp(22973, 2);
+				
+				st.setState(COMPLETED);
+				st.exitCurrentQuest(false);
+				st.playSound(SOUND_FINISH);
+				
+				htmltext = "33952-6.htm";
 			}
 			
 		}
-		else if(npcId == LEIRA)
+		else if(npcId == KIKU_CAVE)
 		{
 			if(cond == 2)
 			{
-				htmltext = "33952-1.htm";
+				htmltext = "33995.htm";
 			}
 		}
 		
