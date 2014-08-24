aab622c63cf9f00aaba5cd59247b248eb5c16995
sontronghien <Hien Son>
2014-08-25 03:43:26 +0700

    Fix despawn script

--- dist/gameserver/data/scripts/npc/model/TurekOrcInstance.java
+++ dist/gameserver/data/scripts/npc/model/TurekOrcInstance.java
@@ -1,5 +1,6 @@
 package npc.model;
 
+import l2s.commons.threading.RunnableImpl;
 import l2s.gameserver.instancemanager.ReflectionManager;
 import l2s.gameserver.model.Creature;
 import l2s.gameserver.model.instances.MonsterInstance;
@@ -12,7 +13,6 @@ import l2s.gameserver.ThreadPoolManager;
 import java.util.concurrent.ScheduledFuture;
 
 import l2s.gameserver.model.instances.NpcInstance;
-import l2s.gameserver.model.quest.Quest.DeSpawnScheduleTimerTask;
 import l2s.gameserver.network.l2.s2c.ExShowScreenMessage;
 
 
@@ -46,5 +46,25 @@ public class TurekOrcInstance extends MonsterInstance
 		if(despawnDelay > 0 && result != null)
 			ThreadPoolManager.getInstance().schedule(new DeSpawnScheduleTimerTask(result), despawnDelay);
 		return result;
+	}
+	
+	public class DeSpawnScheduleTimerTask extends RunnableImpl
+	{
+		NpcInstance _npc = null;
+
+		public DeSpawnScheduleTimerTask(NpcInstance npc)
+		{
+			_npc = npc;
+		}
+
+		@Override
+		public void runImpl() throws Exception
+		{
+			if(_npc != null)
+				if(_npc.getSpawn() != null)
+					_npc.getSpawn().deleteAll();
+				else
+					_npc.deleteMe();
+		}
 	}
 }
