a7ae1303dfe4c0f37be8fe06a34dc6004e09e91b
Kiệt Thành Võ <vo_thanh_kiet@yahoo.com>
2014-09-04 16:48:10 +0700

    Eli traslate

--- dist/gameserver/data/scripts/quests/_10391_ASuspiciousHelper.java
+++ dist/gameserver/data/scripts/quests/_10391_ASuspiciousHelper.java
@@ -1,6 +1,5 @@
 package quests;
 
-import l2s.gameserver.model.Player;
 import l2s.gameserver.model.instances.NpcInstance;
 import l2s.gameserver.model.quest.Quest;
 import l2s.gameserver.model.quest.QuestState;
@@ -8,10 +7,10 @@ import l2s.gameserver.scripts.ScriptFile;
 
 public class _10391_ASuspiciousHelper extends Quest implements ScriptFile
 {
-	private static final int ELLI = 33858;
+	private static final int ELI = 33858;
 	private static final int CHEL = 33861;
 	private static final int IASON_HEINE = 33859;
-	private static final int FAKE_ID = 36707;
+	private static final int FORGE_IDENTIFICATION_CARD = 36707;
 	private static final int MATERIALS = 36708;
     private static final int EAC = 22011;
 	private static final int STEEL_DOOR_GUILD_COIN = 37045;
@@ -20,17 +19,17 @@ public class _10391_ASuspiciousHelper extends Quest implements ScriptFile
 	{
 		super(false);
 
-		addStartNpc(ELLI);
-		addTalkId(ELLI);
+		addStartNpc(ELI);
+		addTalkId(ELI);
 		addTalkId(CHEL);
 		addTalkId(IASON_HEINE);
-		addQuestItem(FAKE_ID);
+		addQuestItem(FORGE_IDENTIFICATION_CARD);
 	}
 
 	@Override
 	public String onEvent(String event, QuestState st, NpcInstance npc)
 	{
-		String htmltext = event;
+		String htmlText = event;
 		if(event.equalsIgnoreCase("barons_personal_escort_eli_q10391_02"))
 			return "barons_personal_escort_eli_q10391_02.htm";
 		else if(event.equalsIgnoreCase("barons_personal_escort_eli_q10391_03"))
@@ -38,7 +37,7 @@ public class _10391_ASuspiciousHelper extends Quest implements ScriptFile
 		else if(event.equalsIgnoreCase("quest_accept"))
 		{
 			st.setCond(1);
-			st.giveItems(FAKE_ID, 1L);
+			st.giveItems(FORGE_IDENTIFICATION_CARD, 1L);
 			st.setState(STARTED);
 			st.playSound(SOUND_ACCEPT);
 			return "barons_personal_escort_eli_q10391_04.htm";
@@ -47,10 +46,10 @@ public class _10391_ASuspiciousHelper extends Quest implements ScriptFile
 			return "worker_chel_q10391_02.htm";
 		else if(event.equalsIgnoreCase("worker_chel_q10391_03"))
 		{
-			if(st.getQuestItemsCount(FAKE_ID) > 0)
+			if(st.getQuestItemsCount(FORGE_IDENTIFICATION_CARD) > 0)
 			{
 				st.setCond(2);
-				st.takeItems(FAKE_ID, 1L);
+				st.takeItems(FORGE_IDENTIFICATION_CARD, 1L);
 				st.giveItems(MATERIALS, 1L);
 				return "worker_chel_q10391_03.htm";	
 			}
@@ -68,32 +67,45 @@ public class _10391_ASuspiciousHelper extends Quest implements ScriptFile
 			st.exitCurrentQuest(false);
 			return "iason_heine_q10391_04.htm";
 		}
-		return htmltext;
+		return htmlText;
 	}
 
 	@Override
 	public String onTalk(NpcInstance npc, QuestState st)
 	{
-		String htmltext = "noquest";
+		String htmlText = "noquest";
 		int npcId = npc.getNpcId();
 		int cond = st.getCond();
-		if(npcId == ELLI)
+		if(npcId == ELI)
 		{
 			if(st.isCompleted())
-				return "This quest is completed";
+            {
+                return "This quest is completed";
+            }
 			else if(cond == 0)
-				return "barons_personal_escort_eli_q10391_01.htm";
-			else	
-				return "barons_personal_escort_eli_q10391_04.htm";
+            {
+                return "barons_personal_escort_eli_q10391_01.htm";
+            }
+			else
+            {
+                return "barons_personal_escort_eli_q10391_04.htm";
+            }
 		}	
 		else if(npcId == CHEL)
-			if(cond == 1)
-				return "worker_chel_q10391_01.htm";
-				
+        {
+            if (cond == 1)
+            {
+                return "worker_chel_q10391_01.htm";
+            }
+        }
 		else if(npcId == IASON_HEINE)
-			if(cond == 2)
-				return "iason_heine_q10391_01.htm"; 
-		return htmltext;
+        {
+            if (cond == 2)
+            {
+                return "iason_heine_q10391_01.htm";
+            }
+        }
+		return htmlText;
 	}
 
 	@Override
