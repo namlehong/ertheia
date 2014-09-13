afc122e7b0852dd2c2ace704d96a111c4cbde39f
sontronghien <Hien Son>
2014-09-13 07:40:47 +0700

    Update warrior AI

--- dist/gameserver/data/scripts/ai/chamber_of_prophecies/NpcWarriorAI.java
+++ dist/gameserver/data/scripts/ai/chamber_of_prophecies/NpcWarriorAI.java
@@ -2,13 +2,10 @@ package ai.chamber_of_prophecies;
 
 import java.util.List;
 
-import l2s.commons.util.Rnd;
-import l2s.gameserver.ai.CtrlEvent;
 import l2s.gameserver.ai.CtrlIntention;
 import l2s.gameserver.ai.Fighter;
+import l2s.gameserver.model.WorldRegion;
 import l2s.gameserver.model.instances.NpcInstance;
-import l2s.gameserver.network.l2.components.NpcString;
-import l2s.gameserver.scripts.Functions;
 import l2s.gameserver.geodata.GeoEngine;
 
 /**
@@ -41,6 +38,32 @@ public class NpcWarriorAI extends Fighter
 		NpcInstance actor = getActor();
 		
 		return startAttack();
+	}
+	
+	@Override
+	protected void returnHome(boolean clearAggro, boolean teleport)
+	{
+		
+	}
+	
+	@Override
+	public void runImpl() throws Exception
+	{
+		if(_aiTask == null)
+			return;
+		
+		if(!isGlobalAI() && System.currentTimeMillis() - _lastActiveCheck > 5000L)
+		{
+			_lastActiveCheck = System.currentTimeMillis();
+			NpcInstance actor = getActor();
+			WorldRegion region = actor == null ? null : actor.getCurrentRegion();
+			if(region == null || !region.isActive())
+			{
+				stopAITask();
+				return;
+			}
+		}
+		onEvtThink();
 	}
 
 	private boolean startAttack()
