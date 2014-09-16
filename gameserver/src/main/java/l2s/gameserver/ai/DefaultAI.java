9585d8a6e6e1557000ac606015876b7339c2709d
sontronghien <Hien Son>
2014-09-17 00:06:11 +0700

    Add more debug trace on DefaultAI

--- gameserver/src/main/java/l2s/gameserver/ai/DefaultAI.java
+++ gameserver/src/main/java/l2s/gameserver/ai/DefaultAI.java
@@ -982,7 +982,12 @@ public class DefaultAI extends CharacterAI
 		
 		//Добавляем только хейт, урон, если атакующий - игровой персонаж, будет добавлен в L2NpcInstance.onReduceCurrentHp
 		actor.getAggroList().addDamageHate(attacker, 0, damage);
-
+		
+		if(actor.getAggroList().get(attacker) != null)
+			System.out.println("dmg " + actor.getAggroList().get(attacker).damage + " hate " + actor.getAggroList().get(attacker).hate);
+		else
+			System.out.println("attacker is not available in the aggro list");
+		
 		// Обычно 1 хейт добавляется хозяину суммона, чтобы после смерти суммона моб накинулся на хозяина.
 		if(damage > 0 && attacker.isServitor())
 			actor.getAggroList().addDamageHate(attacker.getPlayer(), 0, actor.getParameter("searchingMaster", false) ? damage : 1);
