c6620077af402e3352ef2a77904be0304a48765a
sontronghien <Hien Son>
2014-10-07 00:08:24 +0700

    remove the 20% bonus on skill dmg

--- gameserver/src/main/java/l2s/gameserver/stats/Formulas.java
+++ gameserver/src/main/java/l2s/gameserver/stats/Formulas.java
@@ -143,8 +143,8 @@ public class Formulas
 			else if(skill.isSoulBoost())
 				info.damage *= 1.0 + 0.06 * Math.min(attacker.getConsumedSouls(), 5);
 
-			// Ertheia Physical Skill Damage Bonus
-			info.damage *= 1.30113;
+			// Gracia Physical Skill Damage Bonus
+			info.damage *= 1.10113;
 
 			if(info.crit)
 			{
